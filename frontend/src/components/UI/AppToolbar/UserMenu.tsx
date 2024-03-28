30e2d0dd87c851abb8c98da7fcdea61e777aff0f
Temirlan <temadjumanov@gmail.com>
2024-03-28 23:18:28 +0600

    Created initial frontend: UI, users feature, files such as types.d.ts, axios api etc.

---
+++ frontend/src/components/UI/AppToolbar/UserMenu.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Menu,
+  MenuItem,
+  Typography,
+  styled,
+} from "@mui/material";
+import { User } from "../../../types";
+import { useAppDispatch } from "../../../app/hooks";
+import { NavLink } from "react-router-dom";
+import { logout } from "../../../features/users/usersThunk";
+import { apiURL } from "../../../constants";
+
+interface Props {
+  user: User;
+}
+
+const Link = styled(NavLink)({
+  color: "inherit",
+  textDecoration: "none",
+  "&:hover": {
+    color: "inherit",
+  },
+});
+
+const UserMenu: React.FC<Props> = ({ user }) => {
+  const dispatch = useAppDispatch();
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <>
+      <Avatar alt={user.displayName} src={`${apiURL}/${user.avatar}`} />
+      <Button onClick={handleClick} color="inherit">
+        Hello, {user.displayName}
+      </Button>
+
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem>
+          <Typography variant="h6" component="div" sx={{ textAlign: "center" }}>
+            <Link to="/trackHistory">My cocktails</Link>
+          </Typography>
+        </MenuItem>
+        <MenuItem>
+          <Typography variant="h6" component="div" sx={{ textAlign: "center" }}>
+            <Link to="/artists/create">Post new cocktail</Link>
+          </Typography>
+        </MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default UserMenu;
